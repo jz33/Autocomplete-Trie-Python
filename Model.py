3e8f175f7bd71af47702f1e9c42199f8594482b1
Jun <junzhengrice@gmail.com>
2016-01-07 14:05:19 -0600

    Rewrite and implement infix search

--- Model.py
+++ Model.py
@@ -1,26 +1,31 @@
-import Utilities
+from Utilities import LoadFile
 from Trie import TrieNode
-
 '''
 Author: junzhengrice@gmail.com
 '''
 class Model(object):
     def __init__(self):
-        self.root = TrieNode("")
+        self.root = TrieNode()
+        self.PrefixOnly = 1
         
     def Construct(self,filename):
-        buf = Utilities.LoadFile(filename)
+        buf = LoadFile(filename)
         for line in buf:
             line = line.strip()
-            self.root.add(line,0)
+            self.root.add(line)
         buf.close()
  
-    def List(self,tag):
+    def List(self,word):
         container = []
-        self.root.find(tag,0,container)
+        if self.PrefixOnly == 1:
+            self.root.find(word,container)
+        else:
+            self.root.findInfix(word,container)
         return container 
     
-    def Contains(self,tag):
-        tag = tag.strip()
-        res = self.root.contains(tag,0)
-        return res
+    def Contains(self,word):
+        return self.root.contains(word.strip())
+        
+    def SwitchCommand(self):
+        self.PrefixOnly = 1 ^ self.PrefixOnly
+        
